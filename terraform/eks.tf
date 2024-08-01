ba5f1c35f3af115617b0418873f5bc564d99c12f
priyanshu sharma <151720861+cyberdesk07@users.noreply.github.com>
2024-08-01 22:43:08 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -24,7 +24,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    instance_types = ["t2.medium"]
 
     attach_cluster_primary_security_group = true
   }
@@ -32,14 +32,14 @@ module "eks" {
   eks_managed_node_groups = {
     amc-cluster-wg = {
       min_size     = 1
-      max_size     = 2
+      max_size     = 1
       desired_size = 1
 
-      instance_types = ["t3.large"]
+      instance_types = ["t2.medium"]
       capacity_type  = "SPOT"
 
       tags = {
-        ExtraTag = "helloworld"
+        ExtraTag = "Devpriyanshu"
       }
     }
   }
